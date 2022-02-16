96b4f5a9a2b5cf2d1f0c95d68957213bd331466a
Aakash Khapare M <aamanjun@akamai.com>
2022-02-16 19:33:47 +0530

    self bootstrap packer

--- nvim_lua/lua/plugins.lua
+++ nvim_lua/lua/plugins.lua
@@ -29,12 +29,17 @@ vim.call('plug#end')
 
 ]]--
 
--- Packer config
+-- Packer: automatic bootstrap
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
 
-vim.cmd [[packadd packer.nvim]]
-
-return require('packer').startup(function()
-
+return require('packer').startup(function(use)
+  -- My plugins here
+  -- use 'foo1/bar1.nvim'
+  -- use 'foo2/bar2.nvim'
 
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
@@ -42,4 +47,9 @@ return require('packer').startup(function()
     -- gruvbox color scheme
     use "ellisonleao/gruvbox.nvim"
 
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)

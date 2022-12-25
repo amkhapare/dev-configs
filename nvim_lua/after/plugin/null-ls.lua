a8bb7de326ad0560516e737e9fa114e266577a6d
Aakash Khapare M <aamanjun@akamai.com>
2022-12-25 20:40:28 +0530

    Fixed format on save

--- nvim_lua/after/plugin/null-ls.lua
+++ nvim_lua/after/plugin/null-ls.lua
@@ -1,12 +1,30 @@
-local formatting = require("null-ls").builtins.formatting
-local diagnostics = require("null-ls").builtins.diagnostics
-local completion = require("null-ls").builtins.completion
-require("null-ls").setup({
+local null_ls = require("null-ls")
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
+-- local completion = null_ls.builtins.completion
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+null_ls.setup({
     sources = {
         formatting.stylua,
         formatting.black,
         formatting.prettier,
+        formatting.eslint,
         diagnostics.eslint,
-        completion.spell,
+        -- completion.spell,
     },
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                    vim.lsp.buf.format()
+                end,
+            })
+        end
+    end,
 })

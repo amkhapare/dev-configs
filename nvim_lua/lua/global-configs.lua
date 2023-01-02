f046c2a98bd79dc8b314cb039afa65afb2a12067
Aakash Khapare M <aamanjun@akamai.com>
2023-01-03 00:15:44 +0530

    disabled mouse

--- nvim_lua/lua/global-configs.lua
+++ nvim_lua/lua/global-configs.lua
@@ -1,3 +1,4 @@
+vim.o.mouse = false
 --set noerrorbells
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
@@ -10,7 +11,7 @@ vim.o.nu = true
 
 vim.o.wrap = false
 -- vim.o.noswapfile=true
-vim.o.undodir = os.getenv('HOME') .. '/.vim/undodir'
+vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.o.undofile = true
 vim.o.incsearch = true
 vim.o.backspace = "indent,eol,start"
@@ -28,23 +29,22 @@ vim.o.background = "dark" -- or "light" for light mode
 vim.o.colorcolumn = "80"
 
 -- directory explorer
-vim.g.netrw_preview   = 1
+vim.g.netrw_preview = 1
 vim.g.netrw_liststyle = 3
-vim.g.netrw_winsize   = 30
+vim.g.netrw_winsize = 30
 
 --Remap space as leader key
 vim.api.nvim_set_keymap("", "<Space>", "<Nop>", {
-    noremap = true,
-    silent = true
+	noremap = true,
+	silent = true,
 })
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-
-vim.api.nvim_set_keymap('i', 'jj', '<ESC>', {
-    noremap = true,
-    silent = true
+vim.api.nvim_set_keymap("i", "jj", "<ESC>", {
+	noremap = true,
+	silent = true,
 })
 
 -- gruvbox theme

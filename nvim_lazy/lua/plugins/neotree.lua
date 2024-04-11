46a6150d4383fc3986de06b15932201b8917fdff
Aakash Khapare M <aamanjun@akamai.com>
2024-04-11 20:55:21 +0530

    small changes

--- nvim_lazy/lua/plugins/neotree.lua
+++ nvim_lazy/lua/plugins/neotree.lua
@@ -9,6 +9,6 @@ return {
     },
     config = function()
         vim.keymap.set("n", "<C-e>", ":Neotree toggle<CR>", {})
-        vim.keymap.set("n", "<C-r>", ":Neotree reveal<CR>", {})
+        vim.keymap.set("n", "<leader>r", ":Neotree reveal<CR>", {})
     end,
 }
--- nvim_lazy/lua/plugins/none-ls.lua
+++ nvim_lazy/lua/plugins/none-ls.lua
@@ -1,38 +1,37 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+        local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				require("none-ls.formatting.eslint"),
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.prettier,
-				require("none-ls.diagnostics.eslint_d"),
-				null_ls.builtins.completion.spell,
-			},
-			on_attach = function(client, bufnr)
-				local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+                require("none-ls.formatting.eslint"),
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.prettier,
+                require("none-ls.diagnostics.eslint_d"),
+                null_ls.builtins.completion.spell,
+            },
+            on_attach = function(client, bufnr)
+                local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-							print("formatting buf")
-							vim.lsp.buf.format({ bufnr = bufnr })
-						end,
-					})
-				end
-			end,
-		})
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                            vim.lsp.buf.format({ bufnr = bufnr })
+                        end,
+                    })
+                end
+            end,
+        })
 
-		vim.keymap.set("n", "<leader>hf", vim.lsp.buf.format, {})
-	end,
+        vim.keymap.set("n", "<leader>hf", vim.lsp.buf.format, {})
+    end,
 }

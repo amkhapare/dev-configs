4febf32e4f6afe1fedd7aff5fa952d2250715c11
Aakash Khapare M <aamanjun@akamai.com>
2024-04-15 11:24:30 +0530

    neotree changes

--- nvim_lazy/lua/plugins/neotree.lua
+++ nvim_lazy/lua/plugins/neotree.lua
@@ -7,6 +7,22 @@ return {
         "MunifTanjim/nui.nvim",
         -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
+    opts = {
+        filesystem = {
+            filtered_items = {
+                visible = true,
+                show_hidden_count = true,
+                hide_dotfiles = false,
+                hide_gitignored = true,
+                hide_by_name = {
+                    -- '.git',
+                    -- '.DS_Store',
+                    -- 'thumbs.db',
+                },
+                never_show = {},
+            },
+        },
+    },
     config = function()
         vim.keymap.set("n", "<C-e>", ":Neotree toggle<CR>", {})
         vim.keymap.set("n", "<leader>r", ":Neotree reveal<CR>", {})

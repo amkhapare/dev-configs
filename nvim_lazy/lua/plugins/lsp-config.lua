012e68879f560bdfab1e01a5a067bcd4704acdaa
Aakash Khapare M <aamanjun@akamai.com>
2024-04-17 14:05:50 +0530

    remapped to ref

--- nvim_lazy/lua/plugins/lsp-config.lua
+++ nvim_lazy/lua/plugins/lsp-config.lua
@@ -151,7 +151,7 @@ return {
                     vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
                     vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
                     vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-                    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                    vim.keymap.set("n", "tr", vim.lsp.buf.references, opts)
                     vim.keymap.set("n", "<space>f", function()
                         vim.lsp.buf.format({ async = true })
                     end, opts)

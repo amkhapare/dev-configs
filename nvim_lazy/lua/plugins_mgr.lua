a62f4665b2b4f1a42964584a10c2a38968b8affa
Aakash Khapare M <aamanjun@akamai.com>
2024-05-20 10:07:20 +0530

    lazy change detection off

--- nvim_lazy/lua/plugins_mgr.lua
+++ nvim_lazy/lua/plugins_mgr.lua
@@ -1,17 +1,20 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
-local opts = {}
+local opts = {
+    change_detection = {
+        notify = false,
+    },
+}
 
 require("lazy").setup("plugins", opts)
-

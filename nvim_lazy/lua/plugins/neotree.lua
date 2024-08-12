584b84c5d029fc2d32bd428fb86b8d9334d39944
Aakash Khapare M <aamanjun@akamai.com>
2024-08-12 11:57:14 +0530

    few changes

--- nvim_lazy/lua/plugins/neotree.lua
+++ nvim_lazy/lua/plugins/neotree.lua
@@ -11,9 +11,9 @@ return {
         filesystem = {
             filtered_items = {
                 visible = true,
-                -- show_hidden_count = true,
-                -- hide_dotfiles = false,
-                -- hide_gitignored = false,
+                show_hidden_count = true,
+                hide_dotfiles = false,
+                hide_gitignored = false,
                 -- hide_by_name = {
                 --     -- '.git',
                 --     -- '.DS_Store',

b71b267af0c675bd4dd672a536ac3928a44477d0
Aakash Khapare M <aamanjun@akamai.com>
2025-06-06 09:54:28 +0000

    removed grovebox

--- nvim_lazy/lua/plugins/gruvbox.lua
+++ nvim_lazy/lua/plugins/gruvbox.lua
@@ -1,5 +1,9 @@
-return { "ellisonleao/gruvbox.nvim", priority = 1000, config = function()
-    require("gruvbox").setup()
-        
-    -- vim.cmd[[colorscheme gruvbox]]
-end}
+return {
+	-- "ellisonleao/gruvbox.nvim",
+	-- priority = 1000,
+	-- config = function()
+	-- 	require("gruvbox").setup()
+	--
+	-- 	-- vim.cmd[[colorscheme gruvbox]]
+	-- end,
+}

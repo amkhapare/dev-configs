3c30497af2cd6ca1a93b449ca0f4ab3f5288a404
Aakash Khapare M <aamanjun@akamai.com>
2025-06-21 18:22:30 +0000

    neo tree

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,36 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+        -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    ---@module "neo-tree"
+    ---@type neotree.Config?
+    opts = {
+        filesystem = {
+            filtered_items = {
+                visible = true,
+                show_hidden_count = true,
+                hide_dotfiles = false,
+                hide_gitignored = false,
+                -- hide_by_name = {
+                --     -- '.git',
+                --     -- '.DS_Store',
+                --     -- 'thumbs.db',
+                -- },
+                never_show = {},
+                follow_current_file = {
+                    enabled = true,
+                },
+            },
+        },
+    },
+    config = function()
+        vim.keymap.set("n", "<C-e>", ":Neotree toggle<CR>", {})
+        vim.keymap.set("n", "<leader>r", ":Neotree reveal<CR>", {})
+    end,
+}

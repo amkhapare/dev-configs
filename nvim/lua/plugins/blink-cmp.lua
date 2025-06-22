d19b7f5a2896e1f47711fa244a8833c1c2604af4
Aakash Khapare M <aamanjun@akamai.com>
2025-06-22 17:57:19 +0000

    enable method signature preview

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -35,6 +35,7 @@ return {
 
         -- (Default) Only show the documentation popup when manually triggered
         completion = { documentation = { auto_show = false } },
+        signature = { enabled = true },
 
         -- Default list of enabled providers defined so that you can extend it
         -- elsewhere in your config, without redefining it, due to `opts_extend`
